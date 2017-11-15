5cdb7564c0ec6d8851d548cf69473bf102e78470
Raquel Domingo <raqueldomingo@Raquels-MacBook-Air.local>
2017-11-15 10:52:42 -0800

    Initial commit

---
+++ apps/login_reg/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^users$', views.register),
+    url(r'^success$', views.success),
+    url(r'^newuser$', views.newuser),
+    url(r'^login$', views.login)
+]
